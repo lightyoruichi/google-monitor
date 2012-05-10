72f8179acb609fefdc40d930558b4ca2359506f4
Wojciech Tekiela <wojciech@tekiela.pl>
2012-05-11 01:13:57 +0200

    capifony deployment setup

---
+++ app/config/deploy.rb
@@ -0,0 +1,19 @@
+set :application, "google-monitor"
+set :deploy_to,   "/var/www/google-monitor.tekiela.pl"
+set :domain,      "google-monitor.tekiela.pl"
+set :user,        "root"
+
+set :scm,         :git
+set :repository,  "git://github.com/wtekiela/google-monitor.git"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain                         # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Rails migrations will run
+
+set   :use_sudo,      false
+set   :keep_releases, 3
+
+set :shared_files,      ["app/config/parameters.ini"]
+set :shared_children,   [app_path + "/logs", web_path + "/uploads", "vendor"]
+set :update_vendors,    false
+
